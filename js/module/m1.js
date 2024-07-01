4790fb4ddd2c1a9992d73ad06b54007ce7f24ef8
fengfeng <fengfengzhidao@126.com>
2024-07-01 23:31:22 +0800

    前端三件套

---
+++ js/module/m1.js
@@ -0,0 +1,17 @@
+
+export function add(x, y){
+    return x+y
+}
+
+export const name = "枫枫"
+
+export const obj = {
+    name: "fengfeng"
+}
+
+export default {
+    obj: {
+        name: "张三"
+    },
+    add,
+}
